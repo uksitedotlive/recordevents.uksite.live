431ba6fc4615da6ef5ab8e93d00b033c3b680248
Matto9781 <74162167+Matto9781@users.noreply.github.com>
2023-11-28 01:27:57 +0000

    Add files via upload

--- nescotcardsales/2023/gallery.js
+++ nescotcardsales/2023/gallery.js
@@ -8,12 +8,32 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // List of media files (photos and videos)
     const mediaFiles = [
-        'photo1.jpg', 'photo2.jpg', 'photo3.jpg', 'photo9.jpg',
-        
-        'photo5.jpg', 'photo6.jpg', 'photo7.jpg', 'photo8.jpg',
-       
+        'photo1.jpg', 'photo2.jpg', 'photo3.jpg', 'photo4.jpg',
+        'video1.mp4', 'video2.mp4', 'video3.mp4', 'video4.mp4',
+        'photo5.jpg', 'photo7.jpg', 'photo8.jpg',
+        'video5.mp4', 'video6.mp4', 'video7.mp4', 'video8.mp4',
         // Add more files as needed
-'photo4.jpg',
+'photo9.jpg', 'photo10.jpg', 'photo11.jpg',
+
+'photo13.jpg', 'photo14.jpg', 'photo15.jpg',
+
+'photo17.jpg', 'photo18.jpg', 'photo19.jpg', 'photo20.jpg',
+
+'photo21.jpg', 'photo23.jpg', 'photo24.jpg',
+
+'photo25.jpg', 'photo27.jpg', 'photo28.jpg',
+
+'photo29.jpg', 'photo31.jpg', 'photo32.jpg',
+
+ 'photo34.jpg', 'photo35.jpg',
+
+'photo37.jpg', 'photo38.jpg', 'photo40.jpg',
+
+'photo42.jpg', 'photo43.jpg', 
+
+'photo45.jpg', 'photo46.jpg', 'photo47.jpg', 'photo48.jpg',
+
+'photo49.jpg', 'photo51.jpg', 'photo52.jpg',
     ];
 
 
